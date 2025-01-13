29f76974b3244d3dcf57fac73b3bc7d481e3d2da
ikram <samamiikram@gmail.com>
2025-01-13 01:01:03 +0100

    last commit

--- KafkaConsumer/src/main/java/org/example/kafkaconsumer/Kafka/KafkaConsumerConfig.java
+++ KafkaConsumer/src/main/java/org/example/kafkaconsumer/Kafka/KafkaConsumerConfig.java
@@ -16,9 +16,8 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
     }
